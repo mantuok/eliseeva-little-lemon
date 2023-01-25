88095e404cbc81ed94e9d6a830496aa14c56ff6e
mantuok <mantuok@gmail.com>
2023-01-25 14:50:42 -0800

    adaptive navigation in progress

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,40 +1,18 @@
-import React from 'react';
-
-const NavigationList = {
-  Home: {
-    id: 1,
-    name: 'Home',
-    link: '#'
-  },
-  About: {
-    id: 2,
-    name: 'About',
-    link: '#'
-  },
-  Menu: {
-    id: 3,
-    name: 'Menu',
-    link: '#'
-  },
-  Reservation: {
-    id: 4,
-    name: 'Reservation',
-    link: '#'
-  },
-  Order: {
-    id: 5,
-    name: 'Order online',
-    link: '#'
-  },
-  Login: {
-    id: 6,
-    name: 'Login',
-    link: '#'
-  }
-};
+import React, {useState} from 'react';
+import {useMediaQuery} from 'react-responsive';
+import {
+  NavigationList,
+  Viewport,
+  MenuButton
+} from '../../const';
 
 const Navigation = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false)
   const navigationItems = Object.values(NavigationList);
+
+  const handleMenuButtonClick = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  }
 
   const renderNavigationItems = () => {
     return navigationItems.map((item) => {
@@ -42,11 +20,31 @@ const Navigation = () => {
     })
   };
 
-  return (
-    <nav>
+  const isMobile = useMediaQuery({query: `(max-width: ${Viewport.Mobile.max})`});
+
+  const renderMenuButton = (menuButton) => {
+    return (
+      <button 
+        className={menuButton.class}
+        onClick={handleMenuButtonClick}
+      >
+        <span className="visually-hidden">{menuButton.text}</span>
+      </button>
+    )
+  };
+
+  const renderNavigationList = () => {
+    return (
       <ul className="navigation">
         {renderNavigationItems()}
       </ul>
+    )
+  }
+
+  return (
+    <nav>
+      {isMobile && renderMenuButton(MenuButton.Closed)}
+      {isMobileMenuOpen && renderNavigationList()}
     </nav>
   )
 }
---
+++ src/const.js
@@ -0,0 +1,60 @@
+export const Viewport = {
+  Desktop: {
+    name: `desktop`,
+    min: `1024px`
+  },
+  Tablet: {
+    name: `tablet`,
+    min: `768px`,
+    max: `1023px`
+  },
+  Mobile: {
+    name: `mobile`,
+    min: `320px`,
+    max: `767px`
+  }
+};
+
+export const NavigationList = {
+  Home: {
+    id: 1,
+    name: 'Home',
+    link: '#'
+  },
+  About: {
+    id: 2,
+    name: 'About',
+    link: '#'
+  },
+  Menu: {
+    id: 3,
+    name: 'Menu',
+    link: '#'
+  },
+  Reservation: {
+    id: 4,
+    name: 'Reservation',
+    link: '#'
+  },
+  Order: {
+    id: 5,
+    name: 'Order online',
+    link: '#'
+  },
+  Login: {
+    id: 6,
+    name: 'Login',
+    link: '#'
+  }
+};
+
+export const MenuButton = {
+  Open: {
+    text: `Открыть меню`,
+    class: `navigation__open`
+  },
+  Closed: {
+    text: `Закрыть меню`,
+    class: `navigation__closed`
+  }
+};
