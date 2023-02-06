3a529c7fab8f373a0cd7d960a77c5cc11dc5c4c2
mantuok <mantuok@gmail.com>
2023-02-06 11:40:31 -0800

    form styling done

--- src/components/form-step-one/form-step-one.jsx
+++ src/components/form-step-one/form-step-one.jsx
@@ -13,56 +13,58 @@ const FormStepOne = () => {
   return (
     <form className="form">
       <h3 className="form__heading">Step 1 - Enter dinner details</h3>
-      <label className="form__field">
-        <span className="form__label form__label--datepicker">Select date</span>
-        <span className="form__asterisk form__asterisk--datepicker visually-hidden">Mandatory field</span>
-        <DatePicker 
-          portalId="root-portal"
-          className="form__datepicker"
-          placeholderText="Date"
-          selected={startdate} 
-          onChange={(date) => setStartDate(date)} 
-        />
-      </label>
-      <label className="form__field">
-        <span className="form__label form__label--timepicker">Select time</span>
-        <span className="visually-hidden">Mandatory field</span>
-        <TimePicker
-          className="form__timepicker"
-          clearIcon={null}
-          clockIcon={null}
-          format="hh:mm a"
-          disableClock={true}
-          hourPlaceholder="Hours "
-          minutePlaceholder=" Minutes"
-        />
-      </label>
-      <label className="form__field">
-        <span className="form__label">Select occasion</span>
-        <Select 
-          className="form__select"
-          classNamePrefix="react-select"
-          options={occasionList} 
-          unstyled={true}
-          placeholder={Occasion.default.label}
-        />
-      </label>
-      <fieldset className="form__field form__field--radio">
-        <legend className="form__label form__label--radio">Select number of guests</legend>
-        <label htmlFor="one-two">
-          <input className="form__radio__input" type="radio" id="one-two" name="guests" value="one-two" defaultChecked />
-          <span className="form__radio__label">1 - 2</span>
+      <div className="form__wrapper">
+        <label className="form__field">
+          <span className="form__label form__label--datepicker">Select date</span>
+          <span className="form__asterisk form__asterisk--datepicker visually-hidden">Mandatory field</span>
+          <DatePicker 
+            portalId="root-portal"
+            className="form__datepicker"
+            placeholderText="Date"
+            selected={startdate} 
+            onChange={(date) => setStartDate(date)} 
+          />
         </label>
-        <label htmlFor="three-six">
-          <input className="form__radio__input" type="radio" id="three-six" name="guests" value="three-six" />
-          <span className="form__radio__label">3 - 6</span>
+        <label className="form__field">
+          <span className="form__label form__label--timepicker">Select time</span>
+          <span className="visually-hidden">Mandatory field</span>
+          <TimePicker
+            className="form__timepicker"
+            clearIcon={null}
+            clockIcon={null}
+            format="hh:mm a"
+            disableClock={true}
+            hourPlaceholder="Hours "
+            minutePlaceholder=" Minutes"
+          />
         </label>
-        <label htmlFor="more-than-six">
-          <input className="form__radio__input" type="radio" id="more-than-six" name="guests" value="more-than-six" />
-          <span className="form__radio__label">more than 6</span>
+        <label className="form__field">
+          <span className="form__label">Select occasion</span>
+          <Select 
+            className="form__select"
+            classNamePrefix="react-select"
+            options={occasionList} 
+            unstyled={true}
+            placeholder={Occasion.default.label}
+          />
         </label>
-      </fieldset>
-      <MandatoryHint />
+        <fieldset className="form__field form__field--radio">
+          <legend className="form__label form__label--radio">Select number of guests</legend>
+          <label htmlFor="one-two">
+            <input className="form__radio__input" type="radio" id="one-two" name="guests" value="one-two" defaultChecked />
+            <span className="form__radio__label">1 - 2</span>
+          </label>
+          <label htmlFor="three-six">
+            <input className="form__radio__input" type="radio" id="three-six" name="guests" value="three-six" />
+            <span className="form__radio__label">3 - 6</span>
+          </label>
+          <label htmlFor="more-than-six">
+            <input className="form__radio__input" type="radio" id="more-than-six" name="guests" value="more-than-six" />
+            <span className="form__radio__label">more than 6</span>
+          </label>
+        </fieldset>
+        <MandatoryHint />
+      </div>
       <button className="button button--next">Next</button>
     </form>
   )
--- src/components/form-step-two/form-step-two.jsx
+++ src/components/form-step-two/form-step-two.jsx
@@ -1,32 +1,46 @@
 import React from 'react';
 import MandatoryHint from '../mandatory-hint/mandatory-hint';
+import { PatternFormat } from 'react-number-format';
 
 const FormStepTwo = () => {
   return (
     <form className="form">
       <h3 className="form__heading">Step 2 - Enter contact details</h3>
-      <label className="form__field">
-        <span className="form__label form-label--first-name">First Name</span>
-        <span className="visually-hidden">Mandatory field</span>
-        <input className="form__first-name" placeholder="Enter first name..." />
-      </label>
-      <label className="form__field">
-        <span className="form__label form-label--last-name">Last Name</span>
-        <span className="visually-hidden">Mandatory field</span>
-        <input className="form__last-name" placeholder="Enter last name..." />
-      </label>
-      <label className="form__field">
-        <span className="form__label form-label--phone-number">Phone number</span>
-        <span className="visually-hidden">Mandatory field</span>
-        <input className="form__phone-number" type="tel" />
-      </label>
-      <label className="form__field">
-        <span className="form__label">Comment</span>
-        <textarea />
-      </label>
-      <MandatoryHint />
-      <button className="button button--back">Back</button>
-      <button className="button button--submit">Submit</button>
+      <div className="form__wrapper">
+        <label className="form__field">
+          <span className="form__label form-label--first-name">First Name</span>
+          <span className="visually-hidden">Mandatory field</span>
+          <input className="form__first-name" placeholder="Enter first name..." />
+        </label>
+        <label className="form__field">
+          <span className="form__label form-label--last-name">Last Name</span>
+          <span className="visually-hidden">Mandatory field</span>
+          <input className="form__last-name" placeholder="Enter last name..." />
+        </label>
+        <label className="form__field">
+          <span className="form__label form-label--phone-number">Phone number</span>
+          <span className="visually-hidden">Mandatory field</span>
+          <PatternFormat 
+            className="form__phone-number" 
+            name=""  
+            placeholder="Enter phone number..." 
+            id=""
+            format="###-###-####"
+            pattern="[+]{1}[0-9]{1} [0-9]{3}-[0-9]{3}-[0-9]{4}"
+            mask="_"
+            required
+          />
+        </label>
+        <label className="form__field">
+          <span className="form__label">Comment</span>
+          <textarea className="form__textarea" rows="5" />
+        </label>
+        <MandatoryHint />
+      </div>
+      <div className="button__wrapper">
+        <button className="button button--back">Back</button>
+        <button className="button button--submit">Submit</button>
+      </div>
     </form>
   )
 }
